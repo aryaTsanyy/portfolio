1d0a5eed1ef155e43153bb719d606f4866f4b5ab
aryaTsanyy <aryatsanyy@gmail.com>
2023-03-18 19:46:57 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,59 @@
+/**
+ * toggle icon navbar
+ *
+ * @format
+ */
+
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
+
+/**
+ * =================== scroll section active link ====================
+ *
+ * @format
+ */
+
+let section = document.querySelectorAll("section");
+let navlinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+  section.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navlinks.forEach((links) => {
+        links.classList.remove("active");
+        document.querySelector("header nav a[href*=" + id + "]").classList.add("active");
+      });
+    }
+  });
+  /* Sticky Navbar */
+  let header = document.querySelector("header");
+
+  header.classList.toggle("Sticky", window.scrollY > 100);
+
+  /* remove toggle icon and navbar when click navbar link (scroll) */
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
+};
+
+/* Scroll Reveal */
+ScrollReveal({
+  /* reset: true, */
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
+});
+
+ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
+ScrollReveal().reveal(".home-img, .portfolio-box, .contact form", { origin: "bottom" });
+ScrollReveal().reveal(".home-content h1, .about-img", { origin: "left" });
+ScrollReveal().reveal(".home-content p, .about-content", { origin: "right" });
